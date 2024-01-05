1f1d754c60dd0ae3ecb39700c89e29f2290965a6
Paul Ulmer <paulmt00@hs-esslingen.de>
2024-01-05 17:22:47 +0100

    Erste Menüführung hinzugefügt. Einlesen von JSON Dateien möglich und Fehlermeldung falls dieser Vorgang fehlschlägt.

--- mainml.cpp
+++ mainml.cpp
@@ -6,15 +6,65 @@
 
 using json = nlohmann::json;
 
-int main()  {
-    std::cout << "Songbibliothek\n";
+//Song Klasse
+    class Song {
+    public:
+        std::string title;
+        std::string artist;
+        std::string album;
+        std::string release;
+
+        Song(std::string songTitle, std::string songArtist, std::string songAlbum, std::string songRelease) : title(songTitle), artist(songArtist), album(songAlbum), release(songRelease)  {}
+
+
+
+    };
+
+
+
+
+int main() {
+
+// Programmstart -> Einlesen JSON-Datei
+
+    std::cout << "++++++ Songbibliothek ++++++\n++++++                ++++++\n\n";
+    std::cout << "Geben Sie eine Songdatenbibliothek im JSON-Dateiformat an.\nEs muss der ganze Dateinamen mit der Endung '.json' an.\n\n-> ";
+
     std::string songfile;
     std::cin >> songfile;
 
     std::ifstream file(songfile);
 
-    json song;
-    file >> song;
+    if (!file.is_open()) {
+
+        std::cout << "*** Fehler! Die Datei kann nicht geöffnet werden.\nDas Programm wird beendet. ***\n\n\n";
+
+        exit(0);
+
+        return 1;
+    }
+
+    json ml;
+    file >> ml;
+
+    //Erstellen von Song-Objekten aus JSON-Datei
+
+    for (auto& element: ml) {
+
+        std::string title = element["title"];
+        std::string artist = element["artist"];
+        std::string album = element["album"];
+        std::string release = element["release"];
+
+        Song song(title, artist, album, release);
+    }
+
+    file.close();
+
+    std::cout << "Einlesen der JSON-Datei erfolgreich.\n";
+
+
+
 
 
     return 0;
