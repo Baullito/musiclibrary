8fdce30942314c9009baa9cc2001971b47c55319
Paul Ulmer <paulmt00@hs-esslingen.de>
2024-01-05 15:04:31 +0100

    Main Datei anfänge json inplementierung

--- mainml.cpp
+++ mainml.cpp
@@ -1,28 +1,20 @@
 #include <stdio.h>
-/*
-#include songklasse
-#include bibliothek klasse
-#include playlist klasse
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
 
 
-
-
-*/
-
+using json = nlohmann::json;
 
 int main()  {
+    std::cout << "Songbibliothek\n";
+    std::string songfile;
+    std::cin >> songfile;
 
+    std::ifstream file(songfile);
 
-
-
-
-
-
-
-
-
-
-
+    json song;
+    file >> song;
 
 
     return 0;
